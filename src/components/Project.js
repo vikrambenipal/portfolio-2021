a4701e97153069d3206192d2244ef6163b7191e2
vikrambenipal <vikrambenipal@gmail.com>
2021-01-20 11:20:25 -0500

    added bootstrap and working on card components

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import dummyPhoto from '../dummy.png'
+import dummyPhoto from '../dummy.png';
 import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Project = (props) => {
 
     const { title, headline, tools, thumbnail } = props;
     return (
         <div>
-            <Link to={{
-                pathname: `/projects/${title}`,
-                state: {
-                    title,
-                    headline,
-                    tools
-                }
-            }}
-            >More Info</Link>
-            <h3>{title}</h3>
-            <p>{headline}</p>
-            <p>Tools: {tools}</p>
-            <img src={dummyPhoto} style={{ width: '100px' }}/>
+            <Card style={{ width: '20rem' }}>
+                <Card.Img variant="top" src={dummyPhoto} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>{headline}</Card.Text>
+                    <Link to={{
+                        pathname: `/projects/${title}`,
+                        state: {
+                        title,
+                        headline,
+                        tools
+                        }
+                    }}>
+                        <Button variant="primary">Project Information</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
         </div>
     )
 }
