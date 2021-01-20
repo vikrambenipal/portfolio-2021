1830726ad603a993390adc95e0d9dac72ccccb77
vikrambenipal <vikrambenipal@gmail.com>
2021-01-20 14:36:21 -0500

    set up jumbotron

--- src/components/About.js
+++ src/components/About.js
@@ -3,21 +3,17 @@ import Picture from '../me.jpg';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const About = () => {
     return (
         <div>
-            <Container className="mt-5" style={{height: '600px'}}>
-                <Row>
-                    <Col  className="justify-content-center">
-                        <h1>My name is Vikram, nice to meet you!</h1>
-                        <p>I'm a developer who develops things. Check out my projects below!</p>
-                    </Col>
-                    <Col>
-                        <img src={Picture} alt="" style={{ width: '120px'}}/>
-                    </Col>
-                </Row>
-            </Container>
+            <Jumbotron>
+                <h1>My name is Vikram, nice to meet you!</h1>
+                <p>I'm a developer who develops things. Check out my projects below!</p>
+                <img src={Picture} alt="" style={{ width: '120px'}}/>
+            </Jumbotron>
+            
             <h1>Check out some of my projects!</h1>
         </div>
     )
